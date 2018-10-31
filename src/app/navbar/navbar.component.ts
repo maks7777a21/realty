6db1c8b9f1af67c653618c20d18dbfb47e51b859
mopanasenko <opanasenkomax@gmail.com>
2018-11-01 00:46:06 +0200

    default lang ua

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,10 +13,10 @@ export class NavbarComponent {
 
   constructor( public translate: TranslateService) {
       // this language will be used as a fallback when a translation isn't found in the current language
-      translate.setDefaultLang('en');
+      translate.setDefaultLang('ua');
 
        // the lang to use, if the lang isn't available, it will use the current loader to get them
-      translate.use('en');
+      translate.use('ua');
   }
 
   setLang(lang: string) {
