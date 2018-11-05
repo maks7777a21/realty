621944ffb7badef4f7a948de98bc7755ecf73893
mopanasenko <opanasenkomax@gmail.com>
2018-11-05 22:27:10 +0200

    added token verify

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,8 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
   loginUserData = {};
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService, 
+    private _router: Router ) { }
 
   ngOnInit() {
   }
@@ -16,7 +18,11 @@ export class LoginComponent implements OnInit {
   loginUser(){
     this._auth.loginUser(this.loginUserData)
       .subscribe(
-        res => console.log(res),
+        res => {
+          localStorage.setItem('token', res.token);
+          this._router.navigate(['/special']);
+          console.log(res);
+        },
         err => console.log(err)
       );
   }
